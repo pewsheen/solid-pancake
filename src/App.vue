328baf61388d215faf5d6db01455ba70a91b6138
Jason Tsai <jason@pews.dev>
2019-09-22 20:55:48 +0900

    Fix clipboard not working on shorten

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,27 @@
           <v-card-text>
             <v-row>
               <v-col cols="12" sm="6">
-                <v-text-field label="Left Image URL" :rules="[rules.imageUrlMatch]" v-model="leftImgURL" />
+                <v-text-field
+                  label="Left Image URL"
+                  :rules="[rules.imageUrlMatch]"
+                  v-model="leftImgURL"
+                />
               </v-col>
               <v-col cols="12" sm="6">
-                <v-text-field label="Right Image URL" :rules="[rules.imageUrlMatch]" v-model="rightImgURL" />
+                <v-text-field
+                  label="Right Image URL"
+                  :rules="[rules.imageUrlMatch]"
+                  v-model="rightImgURL"
+                />
               </v-col>
               <v-col cols="12" md="7">
                 <v-text-field color="warning" label="Share URL" readonly :value="shareURL"></v-text-field>
               </v-col>
               <v-col cols="4" md="2">
-                <v-btn block depressed x-large color="warning" @click="doCopyClick">
+                <v-btn block depressed x-large color="warning" @click="onCopyClick">
                   Copy
                   <v-icon dark right v-if="copySuccess">mdi-checkbox-marked-circle</v-icon>
+                  <v-icon dark right v-if="copyFailed">mdi-close</v-icon>
                 </v-btn>
               </v-col>
               <v-col cols="8" md="3">
@@ -28,11 +37,11 @@
                   x-large
                   color="success"
                   :loading="loading"
-                  @click="doShorten"
+                  @click="genShortenClick"
                 >
-                  Copy as Shorten
+                  Shorten Link!
                   <v-icon dark right v-if="shortenSuccess">mdi-checkbox-marked-circle</v-icon>
-                  <v-icon dark right v-if="shortenFailed">mdi-cancel</v-icon>
+                  <v-icon dark right v-if="shortenFailed">mdi-close</v-icon>
                 </v-btn>
               </v-col>
             </v-row>
@@ -59,8 +68,10 @@ export default {
     leftImgURL: "",
     rightImgURL: "",
     shareURL: "",
+    shortenURL: "",
     loading: false,
     copySuccess: false,
+    copyFailed: false,
     shortenSuccess: false,
     shortenFailed: false,
     rules: {
@@ -92,38 +103,40 @@ export default {
     }
   },
   methods: {
-    doCopyClick() {
-      this.$copyText(this.shareURL).then(() => {
-        this.copySuccess = true;
-        setTimeout(() => (this.copySuccess = false), 2000);
-      });
+    onCopyClick() {
+      this.$copyText(this.shareURL).then(
+        () => {
+          this.copySuccess = true;
+          setTimeout(() => (this.copySuccess = false), 3000);
+        },
+        () => {
+          this.copyFailed = true;
+          setTimeout(() => (this.copyFailed = false), 3000);
+        }
+      );
     },
-    doShorten() {
+    async genShortenClick() {
       this.loading = true;
-      this.axios
-        .post("https://pews.dev/api/shorten", {
+      let response = {};
+      try {
+        response = await this.axios.post("https://pews.dev/api/shorten", {
           url: this.shareURL
-        })
-        .then(response => {
-          this.loading = false;
-          if (response.status === 200 && response.data && response.data.url) {
-            this.$copyText(response.data.url).then(
-              () => {
-                this.shortenSuccess = true;
-                setTimeout(() => (this.shortenSuccess = false), 5000);
-              },
-              () => {
-                this.shortenFailed = true;
-                setTimeout(() => (this.shortenFailed = false), 5000);
-              }
-            );
-          }
-        })
-        .catch(() => {
-          this.loading = false;
-          this.shortenFailed = true;
-          setTimeout(() => (this.shortenFailed = false), 5000);
         });
+      } catch (e) {
+        this.shortenFailed = true;
+        setTimeout(() => (this.shortenFailed = false), 3000);
+      }
+
+      this.loading = false;
+
+      if (response.status === 200 && response.data && response.data.url) {
+        this.shareURL = response.data.url.replace("http", "https");
+        this.shortenSuccess = true;
+        setTimeout(() => (this.shortenSuccess = false), 3000);
+      } else {
+        this.shortenFailed = true;
+        setTimeout(() => (this.shortenFailed = false), 3000);
+      }
     },
     load() {
       let urlParams = new URLSearchParams(window.location.search);
